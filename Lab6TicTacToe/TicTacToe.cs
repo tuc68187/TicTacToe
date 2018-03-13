002f1f791aa3748dbf2f61fe0b1b460bb69c7c65
unknown <tuc68187@temple.edu>
2018-03-13 19:05:30 -0400

    Lab 6 Submission

---
+++ Lab6TicTacToe/TicTacToe.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6TicTacToe
+{
+    public class TicTacToe
+    {
+        public int player = 1;
+        public int[] newBoard = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public void CreateBoard()
+        {
+            newBoard = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        }
+
+        public void switchPlayer()
+        {
+            if (player == 1)
+            {
+                player = 2;
+            }
+            else if  (player == 2)
+            {
+                player = 1;
+            }
+            CurrentPlayer();
+        }
+
+        public int CurrentPlayer()
+        {
+            return player;
+        }
+
+        public void MakeMove(int space)
+        {
+            if (SpaceInUse(space) == true)
+            {
+                Console.WriteLine("Player cannot move here");
+            }
+            else
+            {
+                newBoard[space] = CurrentPlayer();
+            }
+        }
+
+        public bool SpaceInUse(int i)
+        {
+            if (newBoard[i] != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Lab6TicTacToeTests/TicTacToeClassTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab6TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6TicTacToe.Tests
+{
+    [TestClass()]
+    public class TicTacToeClassTests
+    {
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            TicTacToe board = new TicTacToe();
+            board.CreateBoard();
+
+            Assert.AreEqual(9, board.newBoard.Length);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerX()
+        {
+            TicTacToe board = new TicTacToe();
+            //Player 1 = X
+
+            Assert.AreEqual(1, board.CurrentPlayer());
+
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerO()
+        {
+            TicTacToe board = new TicTacToe();
+            //Player 2 = O
+            board.switchPlayer();
+            Assert.AreEqual(2, board.CurrentPlayer());
+
+        }
+
+        [TestMethod()]
+        public void playerXMarker()
+        {
+            TicTacToe board = new TicTacToe();
+            board.CreateBoard();
+            board.MakeMove(0);
+
+            Assert.AreEqual(1, board.newBoard[0]);
+        }
+
+        [TestMethod()]
+        public void playerOMarker()
+        {
+            TicTacToe board = new TicTacToe();
+            board.CreateBoard();
+            board.switchPlayer();
+            board.MakeMove(0);
+            Assert.AreEqual(2, board.newBoard[0]);
+        }
+
+        [TestMethod()]
+        public void playerMarkerUnoccupied()
+        {
+            TicTacToe board = new TicTacToe();
+            board.CreateBoard();
+            //Space no occupied, false = space is not in use
+            Assert.AreEqual(false, board.SpaceInUse(0));
+        }
+    }
+}
